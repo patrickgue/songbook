3f7cb4015ed58a12e86104cfc3f179994d964270
Patrick Günthard <me@patrickguenthard.ch>
2023-11-14 12:14:09 +0100

    split song file into chord-text tuples

--- songbook.c
+++ songbook.c
@@ -3,6 +3,8 @@
 
 #include "songbook.h"
 
+#define DEBUG
+
 #define BUFF_SIZE 2048
 
 
@@ -10,32 +12,54 @@ int main(int argc, char **argv)
 {
     int i, is_only_chords = 0;
     char buffer[BUFF_SIZE],
-        chords_buffer[BUFF_SIZE],
-        text_buffer[BUFF_SIZE],
-        current_section_name[BUFF_SIZE];
+        chords_buffer[BUFF_SIZE] = "",
+        text_buffer[BUFF_SIZE] = "",
+        current_section_name[BUFF_SIZE],
+        filename[BUFF_SIZE];
 
-    FILE *fd = fopen("test.song", "r");
-
+    FILE *fd;
     struct s_chord_text chords[CHORD_ITEMS_MAX];
 
-    i = 0;
+    strcpy(filename, "hello.song");
+    fd = fopen(filename, "r");
+
+    if (fd == NULL)
+    {
+        fprintf(stderr, "Unable to open file %s" ,filename);
+        return -1;
+    }
+    
+    i = 1;
     
     while (fgets(buffer, BUFF_SIZE, fd) != NULL)
     {
         if (buffer[0] == '#')
         {
+            if (strlen(chords_buffer) > 0)
+            {
+                fprintf(stderr, "Warning: Line of chords overwritten on line %d\n", i);
+            }
+            
             strncpy(chords_buffer, buffer + 2, BUFF_SIZE);
+            chords_buffer[strlen(chords_buffer) - 1] = 0;
         }
 
         if (buffer[0] == '#' && buffer[1] == '#')
         {
             strncpy(chords_buffer, buffer + 3, BUFF_SIZE);
+            chords_buffer[strlen(chords_buffer) - 1] = 0;
             is_only_chords = 1;
         }
 
         if (buffer[0] == '>')
         {
+            if (strlen(text_buffer) > 0)
+            {
+                fprintf(stderr, "Warning: Line of text overwritten on line %d\n", i);
+            }
+            
             strncpy(text_buffer, buffer + 2, BUFF_SIZE);
+            text_buffer[strlen(text_buffer) - 1] = 0;
         }
 
         if (strlen(chords_buffer) > 0 && (strlen(text_buffer) > 0 || is_only_chords))
@@ -45,6 +69,7 @@ int main(int argc, char **argv)
             strcpy(text_buffer, "");
             is_only_chords = 0;
         }
+        i++;
     }
 
     fclose(fd);
@@ -55,12 +80,16 @@ int main(int argc, char **argv)
 
 int songbook_build_chord_list(struct s_chord_text *chords, char *chord_text, char *text)
 {
-    int i, j, indexes[CHORD_ITEMS_MAX], is_inside_chord = 0;
+    int i, j, k, indexes[CHORD_ITEMS_MAX], is_inside_chord = 0;
     j = 0;
 
+#ifdef DEBUG
+    printf("Chords: '%s'\nText:   '%s'\n", chord_text, text);
+#endif
     if (chord_text[0] == ' ')
     {
         indexes[j] = 0;
+        j++;
     }
 
     for (i = 0; i < strlen(chord_text); i++)
@@ -80,11 +109,26 @@ int songbook_build_chord_list(struct s_chord_text *chords, char *chord_text, cha
     
     for (i = 0; i < j && indexes[i] != -1; i++)
     {
-        strncpy(chords[j].chord, chord_text + indexes[i], 32);
-        chords[i].chord[indexes[i + 1] - indexes[i]] = 0;
-        printf("%s / %s ", chord_text + indexes[j], chords[j].chord);
+        strncpy(chords[i].chord, chord_text + indexes[i], 32);
+        k = 0;
+        while (chords[i].chord[k] != ' ')
+            k++;
+        chords[i].chord[k] = 0;
+        if (indexes[i] < strlen(text))
+        {
+            strncpy(chords[i].section, text + indexes[i], CHORD_TEXT_SIZE);
+            if (i < j - 1)
+            {
+                chords[i].section[indexes[i + 1] - indexes[i]] = 0;
+            }
+        }
+#ifdef DEBUG
+        printf("%d: '%s': '%s'\n", i, chords[i].chord, chords[i].section);
+#endif
     }
+#ifdef DEBUG
     printf("\n");
+#endif
 
     return j;
 }
